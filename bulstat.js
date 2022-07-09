9a8565137f9c01fb4f24da4dc0ee360c3aef67b6
Radoslav Stefanov <79109994+RadoStef@users.noreply.github.com>
2022-07-09 13:04:03 +0300

    Update bulstat.js

--- bulstat.js
+++ bulstat.js
@@ -92,12 +92,12 @@ function activateTaco() {
 //some SO code, for copy to clipboard buton :)
 function CopyToClipboard(containerid) {
     if (document.selection) {
-      var range = document.body.createTextRange();
+      let range = document.body.createTextRange();
       range.moveToElementText(document.getElementById(containerid));
       range.select().createTextRange();
       document.execCommand("copy");
     } else if (window.getSelection) {
-      var range = document.createRange();
+      let range = document.createRange();
       range.selectNode(document.getElementById(containerid));
       window.getSelection().addRange(range);
       document.execCommand("copy");
